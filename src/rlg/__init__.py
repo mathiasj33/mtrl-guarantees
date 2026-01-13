bda1eafc198dad63d703ba3b2a092a0cd687f8a0
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2026-01-13 12:11:43 +0000

    Adds paper directory

--- src/rlg/__init__.py
+++ src/rlg/__init__.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 _root = Path(__file__).parent.parent.parent
 MODEL_DIR = _root / "models"
+PAPER_DIR = _root / "paper"
 
-__all__ = ["MODEL_DIR"]
+__all__ = ["MODEL_DIR", "PAPER_DIR"]
